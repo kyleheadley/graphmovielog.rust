be5b34ac569eb1e15656a4b833b7a61b720c42bb
Kyle Headley <kyle.headley@colorado.edu>
2016-02-01 20:47:01 -0700

    minimal implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,110 @@
+//TODO: implement styles
+//TODO: return Results rather than panicing
+//TODO: deal with empty strings
+
 use std::io::prelude::*;
 use std::fs::File;
 
-pub fn startframe(file: &mut File, title: &str, comment: Option<&str>) {
-	write!(file, "[state]").unwrap();
-	let mut lines = title.lines();
-	if let Some(first) = lines.next() {
-		write!(file, "{}", first).unwrap();
-		for l in lines {
-			write!(file, " {}", l).unwrap();
-		}
-	}
-	writeln!(file, "").unwrap();
-	if let Some(comment) = comment {
-		writeln!(file, "{}", comment).unwrap();
-	}
+extern crate time;
+
+pub const DEFAULT_STYLE: &'static str = "active";
+
+pub trait GMLog {
+  fn snapshot(self: &Self, file: &mut File, msg: Option<&str>) {
+    startframe(file, &format!("Logged at {}", time::now().asctime()), msg);
+  }
+  fn comment(self: &Self, file: &mut File, msg: Option<&str>) {
+    makecomment(file, &format!("Commented at {}", time::now().asctime()), msg);
+  }
 }
 
+pub trait GMAutoLog {
+  fn set_file(self: &mut Self, file: &mut File);
+  fn get_file(self: &Self) -> &mut File;
+  fn start(self: &Self, msg: Option<&str>);
+  fn snapshot(self: &Self, msg: Option<&str>);
+  fn comment(self: &Self, msg: Option<&str>);
+  fn end(self: &Self, msg: Option<&str>);
+}
+
+trait CombineStr {
+  fn join(self: &mut Self, sep: &str) -> String;
+} 
+
+impl<'a,I> CombineStr for I where I: Iterator<Item=&'a str> {
+  fn join(self: &mut Self, sep: &str) -> String {
+    let mut result: String = "".to_string();
+    if let Some(part) = self.next() {
+      result = result + part;
+      for part in self {
+        result = result + sep + part;
+      }
+    }
+    result
+  }
+}
+
+fn addtitle(file: &mut File, title: &str, comment: Option<&str>) {
+  let title = title.lines().join(" ");
+  writeln!(file, "{}", title).unwrap();
+  if let Some(comment) = comment {
+    let comment = comment.lines().join("\n ");
+    writeln!(file, " {}", comment).unwrap();
+  }
+}
+
+pub fn startframe(file: &mut File, title: &str, comment: Option<&str>) {
+  write!(file, "[state]").unwrap();
+  addtitle(file, title, comment);
+}
+
+pub fn startdframe(file: &mut File, title: &str, comment: Option<&str>) {
+  write!(file, "[change]").unwrap();
+  addtitle(file, title, comment);
+}
+
+pub fn addnode(file: &mut File, id: &str, style: &str, name: &str, comment: Option<&str>) {
+  //TODO: remove square brackets
+  let id = id.split_whitespace().join("_");
+  let style = style.split_whitespace().join("_");
+  write!(file, "[node {} {}]", id, style).unwrap();
+  addtitle(file, name, comment);
+}
+
+pub fn addedge(file: &mut File, from: &str, to: &str, tag: &str, style: &str, name: &str, comment: Option<&str>) {
+  //TODO: remove square brackets
+  let from = from.split_whitespace().join("_");
+  let to = to.split_whitespace().join("_");
+  let tag = tag.split_whitespace().join("_");
+  let style = style.split_whitespace().join("_");
+  write!(file, "[edge {} {} {} {}]", from, to, tag, style).unwrap();
+  addtitle(file, name, comment);
+}
+
+pub fn makecomment(file: &mut File, short: &str, long: Option<&str>) {
+  write!(file, "[comment]").unwrap();
+  addtitle(file, short, long);
+}
 
 #[test]
 fn it_works() {
-	let mut f = File::create("testlogging.txt").unwrap();
-	startframe(&mut f, &"first".to_string(), None);
-	startframe(&mut f, "one\nmore", Some("then we're done"));
-	startframe(&mut f, "this is it!", Some(" the
-		last
-		one!!!
-		"));
-	startframe(&mut f, "done", None);
+  let f = &mut File::create("testlogging.txt").unwrap();
+  startframe(f, &"first".to_string(), None);
+  startdframe(f, "", Some("no title here?"));
+  startframe(f, "", None);
+  startdframe(f, "one\nmore", Some("then we're done"));
+  startframe(f, "this is it!", Some(" the
+    last
+    one!!!
+    "));
+  startdframe(f, "done", None);
+  addnode(f, "node 1", "blue", "a first node", None);
+  addedge(f, " node 1", "node2", "", "green", "", Some("a green line!\nbeat that ;)"));
+
+  impl GMLog for String {};
+  let l: String = String::from("a logger");
+
+  l.snapshot(f, Some("first snap"));
+  l.snapshot(f, None);
+  l.comment(f, Some("some words"));
 }
